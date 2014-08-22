84eca0a80d3458a6fb78379c570adaec9fb80d59
CamiloMM <herzschaltung@yahoo.com.ar>
2014-08-22 20:39:43 -0300

    Basic Gravatar support.

--- models/user.js
+++ models/user.js
@@ -17,14 +17,28 @@ userSchema.methods.validatePassword = function(password, callback) {
     bcrypt.compare(password, this.hash, function(err, validity) {
         callback(err, validity);
     });
-}
+};
+
+// Generate a gravatar URL. Size is optional.
+userSchema.methods.gravatar = function(size) {
+    // Note that, supposedly, one would need to request from www.gravatar.com,
+    // or even secure.gravatar.com for HTTPS, but this doesn't seem to be
+    // actually necessary. HTTP or HTTPS load from this base URL just fine.
+    var base = '//gravatar.com/avatar/';
+    var params = [
+        's=' + (size || 128),
+        'd=' + 'mm', // "Mystery Man" default image
+        'r=' + 'x' // "Oh my gosh, think of the children!!" - except, no.
+    ];
+    return base + this.md5 + '?' + params.join('&');
+};
 
 // Generate a Gravatar-compatible MD5.
 userSchema.statics.md5 = function(email) {
     var hash = crypto.createHash("md5");
     hash.update(String(email).toLowerCase().trim());
     return hash.digest("hex");
-}
+};
 
 var User = mongoose.model('User', userSchema);
 
