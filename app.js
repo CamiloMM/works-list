e5c44a560691f2c94b04cb290a4bba18c6e6f00e
CamiloMM <herzschaltung@yahoo.com.ar>
2014-08-22 05:49:26 -0300

    Fix bodyParser deprecation.

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.db.once('open', function callback () {
     app.use(favicon(__dirname + '/public/favicon.ico'));
     app.use(logger('dev'));
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded());
+    app.use(bodyParser.urlencoded({extended: true}));
     app.use(cookieParser());
     app.use(session(sessionSettings));
     app.use(app.passport.initialize());
